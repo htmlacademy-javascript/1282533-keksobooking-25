53c2bb454c0961b834b5899756a9f81ae589e871
Keks <keks@htmlacademy.ru>
2022-02-20 19:26:20 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,21 @@
+function randomInteger(min, max) {
+  if (min === max || min > max || min < 0) {
+    // eslint-disable-next-line no-alert
+    alert('Ошибка');
+  } else {
+    const rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+}
+
+function randomFloat(min, max) {
+  if (min === max || min > max || min < 0) {
+    // eslint-disable-next-line no-alert
+    alert('Ошибка');
+  } else {
+    const rand = min + Math.random() * (max + 1 - min);
+    return rand.toFixed(1);
+  }
+}
+randomInteger(1, 5);
+randomFloat (1, 5);
