a26a2ead82d5a52b2e649c3c058588c63601ba6f
blvcksanta <adamjoeblvck@gmail.com>
2022-03-18 11:17:15 +0400

    Валидация вторая часть

--- js/ad-form.js
+++ js/ad-form.js
@@ -36,10 +36,10 @@ function getRoomsErrorMessage () {
     return `${numberRooms.value} комната только для 1 гостя`;
   }
   if (numberRooms.value === '2') {
-    return `${numberRooms.value} комнаты не больше 2х гостей`;
+    return `В ${numberRooms.value} комнаты не больше 2х гостей`;
   }
   if (numberRooms.value === '3') {
-    return `${numberRooms.value} комнаты не больше 3х гостей`;
+    return `В ${numberRooms.value} комнаты не больше 3х гостей`;
   }
   if (numberRooms.value === '100') {
     return `${numberRooms.value} комнат не для гостей`;
@@ -47,6 +47,63 @@ function getRoomsErrorMessage () {
 }
 
 pristine.addValidator(numberGuests, validateRoom, getRoomsErrorMessage);
+
+const apartmentType = adForm.querySelector('#type');
+const apartmentOption = {
+  'bungalow': '0',
+  'flat': '1000',
+  'hotel': '3000',
+  'house': '5000',
+  'palace': '10000',
+};
+
+function apartmentPrice () {
+  price.placeholder = apartmentOption[apartmentType.value];
+  price.min = apartmentOption[apartmentType.value];
+  return !((price.value < +apartmentOption[apartmentType.value]));
+}
+
+function apartmentErrorText () {
+  return `Минимальная цена ${apartmentOption[apartmentType.value]} руб.`;
+}
+
+pristine.addValidator(apartmentType, apartmentPrice, apartmentErrorText);
+
+const timeIn = adForm.querySelector('#timein');
+const timeOut = adForm.querySelector('#timeout');
+
+//Костыль
+timeOut.setAttribute('disabled', 'disabled');
+
+timeIn.addEventListener('change', () => {
+  timeOut[0].removeAttribute('selected', 'selected');
+  if (timeIn.value === '12:00') {
+    timeOut[0].setAttribute('selected', 'selected');
+  }
+  timeOut[1].removeAttribute('selected', 'selected');
+  if (timeIn.value === '13:00') {
+    timeOut[1].setAttribute('selected', 'selected');
+  }
+  timeOut[2].removeAttribute('selected', 'selected');
+  if (timeIn.value === '14:00') {
+    timeOut[2].setAttribute('selected', 'selected');
+  }
+});
+
+// timeOut.addEventListener('change', () => {
+//   timeIn[0].removeAttribute('selected', 'selected');
+//   if (timeOut.value === '12:00') {
+//     timeIn[0].setAttribute('selected', 'selected');
+//   }
+//   timeIn[1].removeAttribute('selected', 'selected');
+//   if (timeOut.value === '13:00') {
+//     timeIn[1].setAttribute('selected', 'selected');
+//   }
+//   timeIn[2].removeAttribute('selected', 'selected');
+//   if (timeOut.value === '14:00') {
+//     timeIn[2].setAttribute('selected', 'selected');
+//   }
+// });
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
